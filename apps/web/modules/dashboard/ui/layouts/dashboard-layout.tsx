b568684ccc85412d0d6f76210e016e6eeaeff7a5
hiverkiya <hiverkiya@gmail.com>
2026-04-09 12:32:40 +0530

    Finalizing Sidebar designing

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -1,7 +1,10 @@
 import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
 import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
-import { SidebarInset, SidebarProvider } from "@workspace/ui/components/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@workspace/ui/components/sidebar";
 import { cookies } from "next/headers";
 export const DashboardLayout = async ({
   children,
