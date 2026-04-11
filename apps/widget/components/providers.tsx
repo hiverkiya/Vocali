8d3609dd3d8b6954980b47bd8d9ee2f7c955b54e
hiverkiya <hiverkiya@gmail.com>
2026-04-11 11:59:35 +0530

    Adding Jotai state management

--- apps/widget/components/providers.tsx
+++ apps/widget/components/providers.tsx
@@ -1,8 +1,13 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
+import * as React from "react";
+import { Provider } from "jotai";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>
+  return (
+    <ConvexProvider client={convex}>
+      <Provider>{children}</Provider>
+    </ConvexProvider>
+  );
 }
