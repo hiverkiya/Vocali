7ce74d2ac909bd6b8831b354a83613801dca9cbf
hiverkiya <hiverkiya@gmail.com>
2026-04-11 13:38:22 +0530

    Persisting state using Jotai, yet to fix minor bugs

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -19,15 +19,23 @@ import { WidgetHeader } from "@/modules/widget/ui/components/widget-header";
 import { useMutation } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Doc } from "@workspace/backend/_generated/dataModel";
+import { useAtomValue, useSetAtom } from "jotai";
+import {
+  contactSessionIdAtomFamily,
+  organizationIdAtom,
+} from "../../atoms/widget-atoms";
 
 const formSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email address"),
 });
 
-const organizationId = "123";
 
 export const WidgetAuthScreen = () => {
+  const organizationId = useAtomValue(organizationIdAtom);
+  const setContactSessionId = useSetAtom(
+    contactSessionIdAtomFamily(organizationId || "")
+  );
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -56,11 +64,12 @@ export const WidgetAuthScreen = () => {
       currentUrl: window.location.href,
     };
 
-    await createContactSession({
+   const contactSessionId= await createContactSession({
       ...values,
       organizationId,
       metadata,
     });
+    setContactSessionId(contactSessionId)
   };
 
   return (
